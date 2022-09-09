42752794c2a5985a973690e0536f95265106cdc8
Robin Svensson <robinesvensson@gmail.com>
2022-09-09 12:46:23 +0200

    -Scaffolded Identity package

--- Labb1_MVCRazor/Program.cs
+++ Labb1_MVCRazor/Program.cs
@@ -1,11 +1,14 @@
 using Labb1_MVCRazor.Models;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Labb1_MVCRazor.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddDefaultIdentity<IdentityUser>().AddEntityFrameworkStores<AppDbContext>();   
 builder.Services.AddScoped<ICustomerRepository, CustomerRepository>();
 builder.Services.AddScoped<IBookRepository, BookRepository>();
 builder.Services.AddScoped<IBookLoanRepository, BookLoanRepository>();
@@ -26,9 +29,12 @@ app.UseStaticFiles();
 app.UseRouting();
 
 app.UseAuthorization();
+app.UseAuthentication();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
